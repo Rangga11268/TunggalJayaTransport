a98b16afe9c85143c318c8960ceb89fe516c2adc
Darell Rangga <darrelrangga@gmail.com>
2025-09-14 15:34:29 +0700

    Fix hamburger menu not working on mobile devices and add responsive navigation for admin panel

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="bg-white shadow">
+<header class="bg-white shadow" x-data="{ open: false }" @click.outside="open = false">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
